681ae53651b33fe45f45d2139ff01972fdddc576
Radhika Mude <radhikamudemain@gmail.com>
2026-04-29 14:54:35 +0530

    explore: string methods practice

---
+++ exploringstrings.py
@@ -0,0 +1,16 @@
+line = "I love anshika, she's world to me and i pormise to prrotect her with all costs "
+c = line.count('a')
+print(c)
+x = line.find('a')
+print(x)
+y = line.replace('pormise', 'promise')
+print(y)
+greet = '    maximum    '
+z = greet.strip()
+print(z)
+print(line.startswith('I'))
+p = line[x:]
+print(p)
+
+d = "Im in love\nher name is anshika"
+print(d)
